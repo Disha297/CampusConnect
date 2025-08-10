dba39d3f505baf88bdacbe1fe881486e384bf3bb
Disha <dishagupta1525@gmail.com>
2025-08-10 16:51:03 +0530

    feat: make change3

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
+require('dotenv').config();
+const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+
 const app = express();
+
 const authRoutes = require('./routes/auth');
 const eventRoutes = require('./routes/events');
 const registerRoutes = require('./routes/register');
 
 // Use an environment variable for the CORS origin.
-// Fallback to local development URL for your local testing.
 const clientUrl = process.env.CLIENT_URL || 'http://localhost:5173';
 app.use(cors({ origin: clientUrl, credentials: true }));
 
@@ -18,7 +21,6 @@ app.use('/api/events', eventRoutes);
 const eventRegistrationsRoute = require('./routes/eventRegistrations');
 app.use('/api', eventRegistrationsRoute);
 
-// Use a dynamic port provided by Render, with a fallback for local testing.
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
